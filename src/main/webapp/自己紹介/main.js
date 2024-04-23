14ca709c5d2394ad357cc05d97fd774ad8db8738
tomboyelf <r.tsuo@ks-japan.jp>
2024-04-23 12:51:26 +0900

    o

---
+++ src/main/webapp/自己紹介/main.js
@@ -0,0 +1,21 @@
+'use strict'
+
+
+function slide() {
+  document.getElementById('book-title').textContent = 'グラスホッパー';
+  document.getElementById('book-about').textContent = '妻の復讐を目論む元教師「鈴木」。自殺専門の殺し屋「鯨」。ナイフ使いの天才「蝉」。３人の思いが交錯するとき、物語は唸りをあげて動き出す。疾走感溢れる筆致で綴られた、分類不能の「殺し屋」小説！';
+  document.getElementById('book-why').textContent = '作品全体のテンポや、最後までつかみきれない不気味なキャラクターの存在が話を面白くしていると感じました。読者が抱くであろう感想を代弁してくれる登場人物がいることも、読みやすさにつながっているように思います。オケの先輩に紹介していただいた作品です。';
+  document.getElementById('book-img').src = 'img/book-4.jpg';
+}
+function slide2() {
+  document.getElementById('book-title').textContent = 'やめるときも、すこやかなるときも';
+  document.getElementById('book-about').textContent = '家具職人の壱晴は、毎年十二月の数日間、声が出なくなる。原因は過去にあったが、誰にも話さず生きてきた。一方、会社員の桜子は困窮する実家を支え、恋とは縁遠い。二人は知人の結婚式で偶然出会い“一夜”を過ごすが、後日、仕事相手として再会し…。欠けた心を抱えた二人が戸惑い、傷つきながらも歩み寄っていく道のりの痛みと温もり。他者と共に生きることのいとおしさに満ちた傑作長編小説。';
+  document.getElementById('book-why').textContent = '男女の視点が交互に書かれた恋愛小説です。透明感のある内容で、どろどろとした展開がなく読みやすい作品でした。とくに、女性作者によって書かれた女性の心理描写は、男性の自分にとって印象深く、記憶に残っています。';
+  document.getElementById('book-img').src = 'img/book-1.jpg';
+}
+function slide3() {
+  document.getElementById('book-title').textContent = 'いちばんやさしい ITパスポート　絶対合格の教科書';
+  document.getElementById('book-about').textContent = '最も売れているITパスポート試験の対策本！圧倒的な高評価で、５年連続売り上げ第１位！絶大な支持を得ているITパスポート試験対策本の令和６年度(2024年度)最新版です！多くの図版を用いた、とことん丁寧な解説と、繰り返し何度も出題されている頻出の過去問（２３２問）を掲載！だから、この一冊で合格できます！';
+  document.getElementById('book-why').textContent = 'ページ数が多すぎると思いました。';
+  document.getElementById('book-img').src = 'img/book-3.jpg';
+}
